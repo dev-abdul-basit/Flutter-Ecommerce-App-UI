e16e390a54be6a3f29ee5d38aa11e65e071ac69e
abdul-basit-dev <abdulbasitkhalid30@gmail.com>
2021-08-23 22:38:25 +0500

    Initial Commit

---
+++ lib/models/Product.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Product({
+    @required this.id,
+    @required this.images,
+    @required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    @required this.title,
+    @required this.price,
+    @required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/ps4_console_blue_1.png",
+      "assets/images/ps4_console_blue_2.png",
+      "assets/images/ps4_console_blue_3.png",
+      "assets/images/ps4_console_blue_4.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Wireless Controller for PS5™",
+    price: 74.99,
+    description: description,
+    rating: 4.9,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/tshirt.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Addidas Yellow - Man Shirt",
+    price: 60.5,
+    description: description,
+    rating: 4.5,
+    isPopular: true,
+  ),
+  Product(
+    id: 3,
+    images: [
+      "assets/images/glap.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Gloves XC Omega - Polygon",
+    price: 46.55,
+    description: description,
+    rating: 4.4,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 4,
+    images: [
+      "assets/images/wireless headset.png",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Logitech Head",
+    price: 20.20,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+  ),
+];
+
+const String description =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s …";
---
+++ lib/screens/home/components/discount_banner.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../../size_config.dart';
+
+class DiscountBanner extends StatelessWidget {
+  const DiscountBanner({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(getProportionateScreenWidth(20)),
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(20),
+        vertical: getProportionateScreenWidth(15),
+      ),
+      decoration: BoxDecoration(
+        // color: Color(0xFF4A3298),
+        color: Color(0xFF6A1B4D),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text.rich(
+        TextSpan(
+          style: TextStyle(color: Colors.white),
+          children: [
+            TextSpan(text: "This Summer Surpise\n"),
+            TextSpan(
+              text: "Cashback upto 30%",
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(24),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
