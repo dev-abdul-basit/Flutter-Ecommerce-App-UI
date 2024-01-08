6d078b3838383cef43b35bd2f80224d8ca9cff58
dev-abdul-basit <dev1abdulbasit@gmail.com>
2024-01-08 14:17:13 +0500

    updated to flutter 3

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'constants.dart';
 
@@ -42,10 +43,7 @@ AppBarTheme appBarTheme() {
   return AppBarTheme(
     color: Colors.white,
     elevation: 0,
-    brightness: Brightness.light,
+    systemOverlayStyle: SystemUiOverlayStyle.dark,
     iconTheme: IconThemeData(color: Colors.black),
-    textTheme: TextTheme(
-      headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
-    ),
   );
 }
